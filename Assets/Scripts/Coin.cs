06e0099fbb0b575694b9f0a2204754e2684e392b
AtmacaSevvall <sevvallatmaca@gmail.com>
2023-10-08 10:22:27 +0300

    coin rotation move

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,4 +2,11 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField]
+    private Vector3 rotationSpeed = new Vector3(0, 100, 0);
+
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+    }
 }
